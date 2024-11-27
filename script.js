8a4442956a34b376a5e6b4f4e76e6de681745b46
bsp3725k <139534037+bsp3725k@users.noreply.github.com>
2024-11-27 15:35:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function firstWord(s) {
   // your code here
+    let str = s.trim().split(" ");
+	return str[0];
 }
-
 // Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
+
+
